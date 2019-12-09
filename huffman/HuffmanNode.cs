cb805bf8cdcfebc5c127b11f39566de5c2393421
mihaly044 <mihaly044@gmail.com>
2019-12-09 23:42:15 +0100

    Print a fancy graph

--- huffman/HuffmanNode.cs
+++ huffman/HuffmanNode.cs
@@ -21,35 +21,116 @@ namespace huffman
         {
             return $"{nameof(Symbol)}: {Symbol}, {nameof(Value)}: {Value}, {nameof(Path)}: {Path.ToBinaryString()}";
         }
+    }
 
-        /// <summary>
-        /// Print this node and all of its children to the console.
-        /// </summary>
-        /// <param name="indent"></param>
-        /// <param name="last"></param>
-        public void Print(string indent = "", bool last = false)
+    public static class HuffmanNodeEx
+    {
+        private class NodeInfo
         {
-            Console.Write(indent);
-            if (last)
-            {
-                Console.Write("└─");
-                indent += "\t";
+            public HuffmanNode Node;
+            public string Text;
+            public int StartPos;
+            public int Size => Text.Length;
+            public int EndPos { 
+                get => StartPos + Size;
+                set => StartPos = value - Size;
             }
-            else
+            public NodeInfo Parent, Left, Right;
+        }
+        
+        /// <summary>
+        /// Prints out a graph of the tree
+        /// Credits: https://stackoverflow.com/a/36496436
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="topMargin"></param>
+        /// <param name="leftMargin"></param>
+        public static void Print(this HuffmanNode root, int topMargin = 2, int leftMargin = 2)
+        {
+            if (root == null) return;
+            var rootTop = Console.CursorTop + topMargin;
+            var last = new List<NodeInfo>();
+            var next = root;
+            for (var level = 0; next != null; level++)
             {
-                Console.Write("├─");
-                indent += "| ";
+                var item = new NodeInfo { Node = next, Text = $"{next.Symbol}({next.Value})"};
+                if (level < last.Count)
+                {
+                    item.StartPos = last[level].EndPos + 1;
+                    last[level] = item;
+                }
+                else
+                {
+                    item.StartPos = leftMargin;
+                    last.Add(item);
+                }
+                if (level > 0)
+                {
+                    item.Parent = last[level - 1];
+                    if (next == item.Parent.Node.Left)
+                    {
+                        item.Parent.Left = item;
+                        item.EndPos = Math.Max(item.EndPos, item.Parent.StartPos);
+                    }
+                    else
+                    {
+                        item.Parent.Right = item;
+                        item.StartPos = Math.Max(item.StartPos, item.Parent.EndPos);
+                    }
+                }
+                
+                next = next.Left ?? next.Right;
+                for (; next == null; item = item.Parent)
+                {
+                    Print(item, rootTop + 2 * level);
+                    if (--level < 0) break;
+                    if (item == item.Parent.Left)
+                    {
+                        item.Parent.StartPos = item.EndPos;
+                        next = item.Parent.Node.Right;
+                    }
+                    else
+                    {
+                        if (item.Parent.Left == null)
+                            item.Parent.EndPos = item.StartPos;
+                        else
+                            item.Parent.StartPos += (item.StartPos - item.Parent.EndPos) / 2;
+                    }
+                }
             }
+            Console.SetCursorPosition(0, rootTop + 2 * last.Count - 1);
+        }
 
-            Console.WriteLine(this);
-            var children = new List<HuffmanNode>();
-            if (Left != null)
-                children.Add(Left);
-            if (Right != null)
-                children.Add(Right);
+        private static void Print(NodeInfo item, int top)
+        {
+            SwapColors();
+            Print(item.Text, top, item.StartPos);
+            SwapColors();
+            if (item.Left != null)
+                PrintLink(top + 1, "┌", "┘", item.Left.StartPos + item.Left.Size / 2, item.StartPos);
+            if (item.Right != null)
+                PrintLink(top + 1, "└", "┐", item.EndPos - 1, item.Right.StartPos + item.Right.Size / 2);
+        }
 
-            for (var i = 0; i < children.Count; i++)
-                children[i].Print(indent, i == children.Count - 1);
+        private static void PrintLink(int top, string start, string end, int startPos, int endPos)
+        {
+            Print(start, top, startPos);
+            Print("─", top, startPos + 1, endPos);
+            Print(end, top, endPos);
+        }
+
+        private static void Print(string s, int top, int left, int right = -1)
+        {
+            Console.SetCursorPosition(left, top);
+            if (right < 0) right = left + s.Length;
+            while (Console.CursorLeft < right) Console.Write(s);
+        }
+
+        private static void SwapColors()
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = Console.BackgroundColor;
+            Console.BackgroundColor = color;
         }
     }
 }
